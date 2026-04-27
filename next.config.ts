9ad9e3b6419b97769c179a88b3f6bbfb87aa4428
Prince Kunal <princekunal652@gmail.com>
2026-04-28 03:06:31 +0530

    or to nullable

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 
-const basePath = process.env.BASE_PATH || "";
+const basePath = process.env.BASE_PATH ?? "";
 
 const nextConfig: NextConfig = {
   output: "export",
